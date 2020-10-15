e2218c1f58b6cade8e71feda5ac80a4b7a6d744f
Siroj <sergy93g@gmail.com>
2020-10-15 19:39:57 +0500

    update (#10)
    * Update build.yml
    
    * Rename .github/workflows/dangerfile.js to dangerfile.js
    
    * mods
    
    * Update build.yml

--- .github/workflows/dangerfile.js
+++ .github/workflows/dangerfile.js
@@ -1,7 +1,13 @@
-import {message, danger} from "danger"
+import {message, danger, warn, fail} from "danger";
 
-const pr = danger.github.pr
-message("Change PR title. Title: " + pr.title)
+const modified = danger.git.modified_files.join("\n");
+message("Changed Files in this PR: \n - " + modified);
 
-const modified = danger.git.modified_files.join("\n")
-message("Changed Files in this PR: \n - " + modified)
+danger.git.commits.forEach(commit => {
+  if (commit.message.length < 5) {
+    warn("Commit name ('${commit.message}') is very short. Name it with definition");
+  }
+  if (commit.message.match(/^(fix)|(mod)/g)) {
+    fail(`Don't use fix or mod on commit messages. Commit: '${commit.message}'`);
+  }
+});
