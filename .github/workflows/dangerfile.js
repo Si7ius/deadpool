23771734cf2529bc419101bad648713c1a9faf41
Siroj <sergy93g@gmail.com>
2020-10-15 16:24:14 +0500

    Update dangerfile.js

--- .github/workflows/dangerfile.js
+++ .github/workflows/dangerfile.js
@@ -1,4 +1,7 @@
 import {message, danger} from "danger"
 
+const pr = danger.github.pr
+message("Change PR title. Title: " + pr.title)
+
 const modified = danger.git.modified_files.join("\n")
 message("Changed Files in this PR: \n - " + modified)
