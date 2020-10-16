a1fc395ce4e57debc101535c6ade5e8329bd965f
Siroj <sergy93g@gmail.com>
2020-10-16 15:58:19 +0500

    Update js_dangerfile.js

--- js_dangerfile.js
+++ js_dangerfile.js
@@ -11,3 +11,35 @@ danger.git.commits.forEach(commit => {
     fail(`Don't use fix or mod on commit messages. Commit: '${commit.message}'`);
   }
 });
+
+// Check for console.log
+const PATTERN = /console\.(log|error|warn|info)/g
+const JS_FILE = /\.(js|html)?$/
+
+const diffs = danger.git.created_files
+  .concat(danger.git.modified_files)
+  .filter(file => JS_FILE.test(file))
+  .map(file => {
+    return danger.git.diffForFile(file).then(diff => ({
+      file,
+      diff,
+    }))
+  });
+
+const additions = await Promise.all(diffs)
+
+additions
+  .filter(({ diff }) => !!diff)
+  .forEach(({ file, diff }) => {
+    const matches = findConsole(diff.added)
+    if (matches.length > 0) callback(file, matches)
+  });
+
+function findConsole(content) {
+  let matches = content.match(PATTERN)
+  return matches || []
+}
+
+function defaultCallback(file, matches) {
+  fail(`remove console in ${file}.`)
+}
