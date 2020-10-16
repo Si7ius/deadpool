369702760b8fee4a5f8df165a703d1981e4fbc23
Siroj <sergy93g@gmail.com>
2020-10-16 17:10:24 +0500

    Update some_file.js

--- some_file.js
+++ some_file.js
@@ -9,9 +9,9 @@
     q.checked.rows = _.filter(checked.rows(), x => !x.pcode);
     q.checked.size = q.checked.rows.length;
     q.checked.has = q.checked.size > 0;
+  console.log('test');
   }
 
-  console.log('test');
   
   if (){
     // if body
