f2f132a2a825bc7f1d7375af80f607b94bc5ab9f
Siroj <sergy93g@gmail.com>
2020-10-16 12:03:43 +0500

    Update js_dangerfile.js

--- js_dangerfile.js
+++ js_dangerfile.js
@@ -5,7 +5,7 @@ message("Changed Files in this PR: \n - " + modified);
 
 danger.git.commits.forEach(commit => {
   if (commit.message.length < 5) {
-    warn("Commit name ('${commit.message}') is very short. Name it with definition");
+    warn(`Commit name ('${commit.message}') is very short. Name it with definition`);
   }
   if (commit.message.match(/^(fix)|(mod)/g)) {
     fail(`Don't use fix or mod on commit messages. Commit: '${commit.message}'`);
